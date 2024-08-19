4c9ccdc5f0da1783d3168e4a07a79527c9677399
Nasr <larko236@outlook.com>
2024-08-19 14:41:01 -0400

    fix: js

--- src/components/dom/WasmContext.tsx
+++ src/components/dom/WasmContext.tsx
@@ -24,7 +24,7 @@ export const WasmProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
   useEffect(() => {
     const initWasm = async () => {
-      const runtime = await import('public/pkg')
+      const runtime = await import('src/pkg')
       setWasmRuntime(runtime)
 
       const newClient = await runtime.createClient({
