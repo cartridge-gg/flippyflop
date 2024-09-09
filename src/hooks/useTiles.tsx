013390dc977581e792741661430e60751fd7678a
Nasr <larko236@outlook.com>
2024-09-09 12:14:16 -0400

    feat: display name in toast

--- src/hooks/useTiles.tsx
+++ src/hooks/useTiles.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { ToriiClient } from '@dojoengine/torii-wasm'
 import { Powerup, Tile as TileModel } from 'src/models'
 import { fetchAllEntities, formatAddress, maskAddress, parseTileModel } from 'src/utils'
@@ -38,33 +38,37 @@ export function useTiles(client: ToriiClient | undefined) {
     })
   }, [client])
 
-  const handleEntityUpdate = async (_hashed_keys: string, entity: any) => {
-    if (entity[TILE_MODEL_TAG]) {
-      const tile = parseTileModel(entity[TILE_MODEL_TAG])
-      const nick = tile.address !== '0x0' ? (usernamesCache?.[tile.address] ?? formatAddress(tile.address)) : 'robot'
-      const isMe = tile.address === (address ? maskAddress(address) : undefined)
+  const handleEntityUpdate = useCallback(
+    async (_hashed_keys: string, entity: any) => {
+      if (entity[TILE_MODEL_TAG]) {
+        const tile = parseTileModel(entity[TILE_MODEL_TAG])
+        const nick = tile.address !== '0x0' ? (usernamesCache?.[tile.address] ?? formatAddress(tile.address)) : 'robot'
+        const isMe = tile.address === (address ? maskAddress(address) : undefined)
 
-      toast(
-        <div className={`flex ${isMe ? 'text-[#F38333]' : 'text-white'} flex-row items-start w-full gap-3`}>
-          <div className='text-current'>
-            {tile.address !== '0x0' ? '🐹' : '👹'} <span className='font-bold text-current'>{isMe ? 'you' : nick}</span>{' '}
-            {tile.address !== '0x0' ? 'flipped' : 'unflipped'} a tile
-            {tile.powerup !== Powerup.None && ` with a ${Powerup[tile.powerup]} powerup`}.
-          </div>
-          <div className='flex-grow'></div>
-          <div
-            className='flex px-1 justify-center items-center gap-2 rounded-s text-current'
-            style={{
-              background: 'rgba(255, 255, 255, 0.10)',
-            }}
-          >
-            X {tile.x}, Y {tile.y}
-          </div>
-        </div>,
-      )
-      setTiles((prev) => ({ ...prev, [`${tile.x},${tile.y}`]: tile }))
-    }
-  }
+        toast(
+          <div className={`flex ${isMe ? 'text-[#F38333]' : 'text-white'} flex-row items-start w-full gap-3`}>
+            <div className='text-current'>
+              {tile.address !== '0x0' ? '🐹' : '👹'}{' '}
+              <span className='font-bold text-current'>{isMe ? 'you' : nick}</span>{' '}
+              {tile.address !== '0x0' ? 'flipped' : 'unflipped'} a tile
+              {tile.powerup !== Powerup.None && ` with a ${Powerup[tile.powerup]} powerup`}.
+            </div>
+            <div className='flex-grow'></div>
+            <div
+              className='flex px-1 justify-center items-center gap-2 rounded-s text-current'
+              style={{
+                background: 'rgba(255, 255, 255, 0.10)',
+              }}
+            >
+              X {tile.x}, Y {tile.y}
+            </div>
+          </div>,
+        )
+        setTiles((prev) => ({ ...prev, [`${tile.x},${tile.y}`]: tile }))
+      }
+    },
+    [address, usernamesCache],
+  )
 
   return { tiles, setTiles }
 }
