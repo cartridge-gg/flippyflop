f97a9c2c9c959d2d72fef7b976969eaf6833b8ad
Nasr <larko236@outlook.com>
2024-09-16 20:23:08 -0400

    feat: 256x256 world

--- src/constants.ts
+++ src/constants.ts
@@ -4,15 +4,15 @@ export const TORII_RPC_URL = 'https://api.cartridge.gg/x/starknet/sepolia'
 // export const TORII_RPC_URL = 'http://localhost:5050'
 export const TORII_RELAY_URL = '/ip4/127.0.0.1/udp/9091/webrtc-direct'
 
-export const WORLD_ADDRESS = '0x53b7efae79ce1d7729828bb6dee2cee09358fde4c4325805cf97678919a4855'
+export const WORLD_ADDRESS = '0x43cba5bc94f3ec38aee9841c8c45edf899e77ac944a88eb732b36ef3a4c2ca2'
 export const TILE_MODEL_TAG = 'flippyflop-Tile'
 
-export const CHUNK_SIZE = 20
-export const CHUNKS = 10000 / (CHUNK_SIZE * CHUNK_SIZE)
+export const CHUNK_SIZE = 16
+export const CHUNKS = 65536 / (CHUNK_SIZE * CHUNK_SIZE)
 export const CHUNKS_PER_DIMENSION = Math.sqrt(CHUNKS)
 export const WORLD_SIZE = CHUNKS_PER_DIMENSION * CHUNK_SIZE
 
-export const ACTIONS_ADDRESS = '0x04a38587ad1b4677196d855f0ac994b401da32c379a728f51df2ea4d13f4a8bb'
+export const ACTIONS_ADDRESS = '0x0165A91F138a5C5f5016a0afE3412b551559B3DE4D89357282fe145E3E3c404b'
 
 export const TILE_ROBOT_SIDE_COLOR = '#14212E'
-export const TILE_SMILEY_SIDE_COLOR = '#a86448'
+export const TILE_SMILEY_SIDE_COLOR = '#ffffff'
