55eff6c6d2a182e1d73c756e8eecdbc102a9cc34
Nasr <larko236@outlook.com>
2024-10-03 16:57:03 -0400

    feat: animate scorebar

--- src/components/dom/Scorebar.tsx
+++ src/components/dom/Scorebar.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RobotIcon from './RobotIcon'
 import UserIcon from './UserIcon'
+
+const NumberTicker = ({ value }) => {
+  const [displayValue, setDisplayValue] = useState(0)
+
+  useEffect(() => {
+    const duration = 1000
+    const valueDelta = Math.abs(value - displayValue)
+    const steps = Math.min(Math.max(Math.ceil(valueDelta / 2), 5), 30)
+    const stepValue = (value - displayValue) / steps
+    let currentStep = 0
+
+    const timer = setInterval(() => {
+      currentStep++
+      setDisplayValue((prevValue) => {
+        const newValue = prevValue + stepValue
+        return currentStep === steps ? value : Math.round(newValue)
+      })
+
+      if (currentStep === steps) {
+        clearInterval(timer)
+      }
+    }, duration / steps)
+
+    return () => clearInterval(timer)
+  }, [value])
+
+  return <span>{displayValue.toLocaleString()}</span>
+}
 
 const Scorebar = ({ humansScore, botsScore, className }) => {
   const totalScore = humansScore + botsScore
@@ -16,19 +44,21 @@ const Scorebar = ({ humansScore, botsScore, className }) => {
     >
       <UserIcon />
       <span className='text-white -ml-1 text-right transition-all duration-300 ease-in-out'>
-        {humansScore.toLocaleString()}
+        <NumberTicker value={humansScore} />
       </span>
       <div className='flex grow h-4 rounded-sm overflow-hidden gap-1'>
         <div
-          className='bg-[#F38332] h-full rounded-sm transition-all duration-300 ease-in-out'
+          className='bg-[#F38332] h-full rounded-sm transition-all duration-1000 ease-in-out'
           style={{ width: `${humansPercentage}%` }}
         ></div>
         <div
-          className='bg-[#52585E] h-full rounded-sm transition-all duration-300 ease-in-out'
+          className='bg-[#52585E] h-full rounded-sm transition-all duration-1000 ease-in-out'
           style={{ width: `${botsPercentage}%` }}
         ></div>
       </div>
-      <span className='text-white transition-all duration-300 ease-in-out'>{botsScore.toLocaleString()}</span>
+      <span className='text-white transition-all duration-300 ease-in-out'>
+        <NumberTicker value={botsScore} />
+      </span>
       <RobotIcon />
     </div>
   )
