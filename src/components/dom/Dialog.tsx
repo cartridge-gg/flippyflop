4579a9430e8381ab9126314d0e331dcbf79d215f
Nasr <larko236@outlook.com>
2024-10-24 18:38:50 -0400

    claim tokens dialog

---
+++ src/components/dom/Dialog.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+interface DialogProps {
+  isOpen: boolean
+  onClose: () => void
+  children: React.ReactNode
+  color?: string
+}
+
+const Dialog = ({ isOpen, onClose, children, color = '#ffffff' }: DialogProps) => {
+  return (
+    <AnimatePresence mode='wait'>
+      {isOpen && (
+        <div className='fixed inset-0 flex items-center justify-center'>
+          <motion.div
+            key='backdrop'
+            className='absolute inset-0 bg-black bg-opacity-60'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: 'easeOut' }}
+            style={{
+              pointerEvents: 'auto',
+              cursor: 'auto',
+            }}
+            onClick={onClose}
+          />
+          <motion.div
+            key='dialog'
+            className='relative w-screen h-screen md:max-h-[30vh] md:w-[90vw] md:max-w-2xl overflow-auto rounded-none md:rounded-lg px-4 pb-3 pt-20 md:pt-4 text-white backdrop-blur bg-black/60'
+            style={{
+              border: `1px dashed ${color}`,
+              boxShadow: `0px 0px 5px 0px ${color}`,
+            }}
+            initial={{ scale: 0.95, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.95, opacity: 0 }}
+            transition={{ duration: 0.2, ease: 'easeOut' }}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Dialog
--- src/components/dom/Header.tsx
+++ src/components/dom/Header.tsx
@@ -14,6 +14,14 @@ import { TEAMS, TILE_REGISTRY, WORLD_SIZE } from '@/constants'
 import { toast } from 'sonner'
 import { Powerup, Tile } from '@/models'
 import { maskAddress } from '@/utils'
+import Dialog from './Dialog'
+import { Canvas } from '@react-three/fiber'
+import TileInstances from '../canvas/InstancedTiles'
+import tileShader from '@/shaders/tile.shader'
+import { TextureLoader, SRGBColorSpace, MeshBasicMaterial, MeshStandardMaterial } from 'three'
+import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
+import { OrbitControls, OrthographicCamera, PerspectiveCamera } from '@react-three/drei'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
 
 interface HeaderProps {
   tiles: Record<string, Tile>
@@ -72,7 +80,69 @@ const Header: React.FC<HeaderProps> = ({ tiles, tps, leaderboard, isLoading, sel
     }, 0)
 
   const [leaderboardOpenedMobile, setLeaderboardOpenedMobile] = useState(false)
+  const [claimDialogOpen, setClaimDialogOpen] = useState(false)
   const [clickedTeam, setClickedTeam] = useState<string | null>(null)
+
+  const textures = useMemo(() => {
+    const loader = new TextureLoader()
+    const loadTexture = (url: string) => {
+      const tex = loader.load(url)
+      tex.colorSpace = SRGBColorSpace
+      return tex
+    }
+
+    return {
+      robot: loadTexture(TILE_REGISTRY.robot.texture),
+      orange: loadTexture(TILE_REGISTRY.orange.texture),
+      green: loadTexture(TILE_REGISTRY.green.texture),
+      red: loadTexture(TILE_REGISTRY.red.texture),
+      blue: loadTexture(TILE_REGISTRY.blue.texture),
+      pink: loadTexture(TILE_REGISTRY.pink.texture),
+      purple: loadTexture(TILE_REGISTRY.purple.texture),
+      bonusOrange: loadTexture(TILE_REGISTRY.orange.bonusTexture),
+      bonusGreen: loadTexture(TILE_REGISTRY.green.bonusTexture),
+      bonusRed: loadTexture(TILE_REGISTRY.red.bonusTexture),
+      bonusBlue: loadTexture(TILE_REGISTRY.blue.bonusTexture),
+      bonusPink: loadTexture(TILE_REGISTRY.pink.bonusTexture),
+      bonusPurple: loadTexture(TILE_REGISTRY.purple.bonusTexture),
+    }
+  }, [])
+
+  const robotMaterial = useMemo(
+    () =>
+      new MeshBasicMaterial({
+        transparent: true,
+        map: textures.robot,
+      }),
+    [textures.robot],
+  )
+  const material = useMemo(() => {
+    const baseMaterial = new MeshBasicMaterial({
+      transparent: true,
+    })
+    return new CustomShaderMaterial({
+      baseMaterial,
+      vertexShader: tileShader.vertex,
+      fragmentShader: tileShader.fragment,
+      uniforms: {
+        robotTexture: { value: textures.robot },
+        orangeTexture: { value: textures.orange },
+        greenTexture: { value: textures.green },
+        redTexture: { value: textures.red },
+        blueTexture: { value: textures.blue },
+        pinkTexture: { value: textures.pink },
+        purpleTexture: { value: textures.purple },
+        bonusOrangeTexture: { value: textures.bonusOrange },
+        bonusGreenTexture: { value: textures.bonusGreen },
+        bonusRedTexture: { value: textures.bonusRed },
+        bonusBlueTexture: { value: textures.bonusBlue },
+        bonusPinkTexture: { value: textures.bonusPink },
+        bonusPurpleTexture: { value: textures.bonusPurple },
+        time: { value: 0 },
+      },
+      transparent: true,
+    })
+  }, [textures])
 
   const scores = useMemo(() => {
     const scores = Object.fromEntries(Object.values(TEAMS).map((team) => [team, 0]))
@@ -96,7 +166,12 @@ const Header: React.FC<HeaderProps> = ({ tiles, tps, leaderboard, isLoading, sel
             <TPS tps={tps} />
             <TeamSelector className='hidden lg:flex' selectedTeam={selectedTeam} setSelectedTeam={setSelectedTeam} />
           </div>
-          <Scorebar className={'w-full'} scores={scores} selectedTeam={selectedTeam} />
+          <Scorebar
+            className={'w-full pointer-events-auto'}
+            scores={scores}
+            selectedTeam={selectedTeam}
+            onClick={() => setLeaderboardOpenedMobile((prev) => !prev)}
+          />
         </div>
         <div className='flex w-full md:w-2/5 md:max-w-96 flex-col gap-4'>
           <div className='pointer-events-auto flex gap-3'>
@@ -106,7 +181,9 @@ const Header: React.FC<HeaderProps> = ({ tiles, tps, leaderboard, isLoading, sel
               outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
               icon={<CoinsIcon />}
               text={userScore.toString()}
-              onClick={() => setLeaderboardOpenedMobile((prev) => !prev)}
+              onClick={() => {
+                setClaimDialogOpen(true)
+              }}
             />
             <OutlineButton
               className='w-full'
@@ -141,6 +218,75 @@ const Header: React.FC<HeaderProps> = ({ tiles, tps, leaderboard, isLoading, sel
           />
         </div>
       </div>
+      <Dialog
+        isOpen={claimDialogOpen}
+        onClose={() => setClaimDialogOpen(false)}
+        color={TILE_REGISTRY[TEAMS[selectedTeam]].border}
+      >
+        <div className='flex flex-col w-full h-full gap-2 items-start pointer-events-auto'>
+          <h1 className='text-2xl font-bold'>Claim</h1>
+          <p className='text-md'>
+            During the entire duration of the game, each flipped tile will earn you potential $FLIP tokens. There are
+            two types of tiles: Powerup and normal tiles. Powerup tiles are rarer and unflippable, while normal tiles
+            can get flipped back by bots.
+          </p>
+          <div className='flex flex-row w-full h-full items-center md:px-20'>
+            <span
+              style={{
+                color: TILE_REGISTRY[TEAMS[selectedTeam]].background,
+              }}
+              className='text-md flex-1 text-center'
+            >
+              Earns 2x, 4x, 8x ... 32x $FLIP
+            </span>
+            <div className='ml-8 flex-1'>
+              <Canvas gl={{}} className='' style={{ height: '150px', width: '240px' }}>
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[10, 10, 10]} intensity={1} />
+                <group rotation={[Math.PI / 3, 0, Math.PI / 8]}>
+                  <TileInstances
+                    tiles={[
+                      { address: '0x1', x: 0, y: 0, team: selectedTeam, powerup: Powerup.Multiplier, powerupValue: 1 },
+                      { address: '0x2', x: 0.9, y: 0, team: selectedTeam, powerup: Powerup.None, powerupValue: 1 },
+                    ]}
+                    position={[-0.6, -0.1, 0]}
+                    material={material}
+                    robotMaterial={robotMaterial}
+                  />
+                </group>
+                <OrthographicCamera makeDefault zoom={70} position={[0, 0, 0]} near={0} far={100000000} />
+                <EffectComposer>
+                  <Bloom
+                    blendFunction={10}
+                    kernelSize={5}
+                    intensity={0.6}
+                    luminanceThreshold={0.4}
+                    luminanceSmoothing={1.5}
+                    resolutionScale={0.1}
+                  />
+                </EffectComposer>
+              </Canvas>
+            </div>
+            <span className='text-md flex-1 text-center'>Earns 1 $FLIP</span>
+          </div>
+          <div className='h-full' />
+          <div className='flex flex-row w-full gap-2 justify-center'>
+            <OutlineButton
+              outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
+              className='w-full md:w-1/3'
+              text='Close'
+              onClick={() => setClaimDialogOpen(false)}
+            />
+            <OutlineButton
+              outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
+              className='w-full md:w-1/3'
+              text={`Claim ${userScore} $FLIP`}
+              disabled
+              onClick={() => setClaimDialogOpen(false)}
+            />
+          </div>
+        </div>
+      </Dialog>
     </div>
   )
 }
