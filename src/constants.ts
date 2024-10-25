d96ec6bb9964f62eed09aa49cb31aa802afac858
Nasr <larko236@outlook.com>
2024-10-25 17:54:36 -0400

    update rpc url

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import manifest from '../contracts/manifests/sepolia/deployment/manifest.json'
 
 export const TORII_URL = 'https://t.nsrdm.com'
 // sepolia
-export const TORII_RPC_URL = 'https://api.cartridge.gg/x/starknet/mainnet'
+export const TORII_RPC_URL = 'https://api.cartridge.gg/x/starknet/sepolia'
 // katana
 // export const TORII_RPC_URL = 'https://api.cartridge.gg/x/flippyflop/katana'
 export const TORII_RELAY_URL = '/ip4/127.0.0.1/udp/9091/webrtc-direct'
