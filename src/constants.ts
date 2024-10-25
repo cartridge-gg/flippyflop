840954bc0ed7998131ad5ce197f867b994aab4d2
steebchen <contact@luca-steeb.com>
2024-10-25 19:16:27 +0100

    test 2

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import manifest from '../flippyflop/manifests/mainnet/deployment/manifest.json'
 
-export const TORII_URL = 'https://api.cartridge.gg/x/eternum-42/torii'
+export const TORII_URL = 'https://api.cartridge.gg/x/ls-tmpx-1/torii'
 // sepolia
 export const TORII_RPC_URL = 'https://api.cartridge.gg/x/starknet/mainnet'
 // katana
