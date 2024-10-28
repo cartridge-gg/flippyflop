aa8072755089e6267668194a3593db7eaaff5c94
Nasr <larko236@outlook.com>
2024-10-27 20:31:42 -0400

    fix: claim dialog

--- src/components/dom/ClaimDialog.tsx
+++ src/components/dom/ClaimDialog.tsx
@@ -1,11 +1,13 @@
 import { OrthographicCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
-import React, { useMemo } from 'react'
+import { useAccount } from '@starknet-react/core'
+import React, { useMemo, useState } from 'react'
 import { toast } from 'sonner'
 import { MeshBasicMaterial, SRGBColorSpace, TextureLoader } from 'three'
 import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
 
+import ClaimSuccessDialog from './ClaimSuccessDialog'
 import Dialog from './Dialog'
 import OutlineButton from './OrangeButton'
 import TileInstances from '../canvas/InstancedTiles'
@@ -24,8 +26,6 @@ interface ClaimDialogProps {
   timeRange: [number, number]
   claimed: bigint
   userScore: number
-  address?: string
-  account?: any
   tiles: Record<string, Tile>
 }
 
@@ -36,10 +36,11 @@ const ClaimDialog: React.FC<ClaimDialogProps> = ({
   timeRange,
   claimed,
   userScore,
-  address,
-  account,
   tiles,
 }) => {
+  const { address, account } = useAccount()
+  const [showSuccess, setShowSuccess] = useState(false)
+
   const textures = useMemo(() => {
     const loader = new TextureLoader()
     const loadTexture = (url: string) => {
@@ -102,117 +103,129 @@ const ClaimDialog: React.FC<ClaimDialogProps> = ({
   }, [textures])
 
   return (
-    <Dialog isOpen={isOpen} onClose={onClose} color={TILE_REGISTRY[TEAMS[selectedTeam]].border}>
-      <div className='flex flex-col w-full h-full gap-2 items-start pointer-events-auto'>
-        <div className='flex flex-row w-full justify-between items-center'>
-          <h1 className='text-2xl font-bold'>Claim</h1>
-          <span className='text-sm opacity-80 animate-pulse'>
-            {Date.now() / 1000 < timeRange[0]
-              ? `Game starts in ${formatEta(timeRange[0])}*`
-              : Date.now() / 1000 > timeRange[1]
-                ? 'Game has ended'
-                : `Game ends in ${formatEta(timeRange[1])}*`}
-          </span>
-        </div>
-        <p className='text-md'>
-          During the entire duration of the game, each flipped tile will earn you potential $FLIP tokens. There are two
-          types of tiles: Powerup and normal tiles. Powerup tiles are rarer and unflippable, while normal tiles can get
-          flipped back by bots. <br />
-          <br />
-          Once the game is over, you will be able to claim your $FLIP tokens.
-        </p>
-        <div className='flex flex-row w-full h-full/3 items-center md:px-20'>
-          <span
-            style={{
-              color: TILE_REGISTRY[TEAMS[selectedTeam]].background,
-              textShadow: `0 0 10px ${TILE_REGISTRY[TEAMS[selectedTeam]].background},
-                          0 0 20px ${TILE_REGISTRY[TEAMS[selectedTeam]].background},
-                          0 0 30px ${TILE_REGISTRY[TEAMS[selectedTeam]].background}`,
-              animationDuration: '3s',
-            }}
-            className='text-sm md:text-md flex-1 text-center animate-pulse'
-          >
-            Earns 2x, 4x, 8x ... 32x $FLIP
-          </span>
-          <div className='ml-8 flex-1'>
-            <Canvas
-              gl={{
-                pixelRatio: window.devicePixelRatio,
-              }}
-              className=''
-              style={{ height: '150px', width: '200px' }}
-            >
-              <ambientLight intensity={0.5} />
-              <directionalLight position={[10, 10, 10]} intensity={1} />
-              <group rotation={[Math.PI / 3, 0, Math.PI / 8]}>
-                <TileInstances
-                  tiles={[
-                    { address: '0x1', x: 0, y: 0, team: selectedTeam, powerup: Powerup.Multiplier, powerupValue: 1 },
-                    { address: '0x2', x: 0.9, y: 0, team: selectedTeam, powerup: Powerup.None, powerupValue: 1 },
-                  ]}
-                  position={[-0.6, -0.1, 0]}
-                  material={material}
-                  robotMaterial={robotMaterial}
-                />
-              </group>
-              <OrthographicCamera makeDefault zoom={70} position={[0, 0, 0]} near={0} far={100000000} />
-              <EffectComposer>
-                <Bloom
-                  kernelSize={4}
-                  intensity={1.1}
-                  luminanceThreshold={0.4}
-                  luminanceSmoothing={1.5}
-                  resolutionScale={0.1}
-                />
-              </EffectComposer>
-            </Canvas>
+    <>
+      <Dialog isOpen={isOpen} onClose={onClose} color={TILE_REGISTRY[TEAMS[selectedTeam]].border}>
+        <div className='flex flex-col w-full h-full gap-2 items-start pointer-events-auto'>
+          <div className='flex flex-row w-full justify-between items-center'>
+            <h1 className='text-2xl font-bold'>Claim</h1>
+            <span className='text-sm opacity-80 animate-pulse'>
+              {Date.now() / 1000 < timeRange[0]
+                ? `Game starts in ${formatEta(timeRange[0])}*`
+                : Date.now() / 1000 > timeRange[1]
+                  ? 'Game has ended'
+                  : `Game ends in ${formatEta(timeRange[1])}*`}
+            </span>
           </div>
-          <span className='text-sm md:text-md flex-1 text-center'>
-            Earns 1 <span style={{ color: TILE_REGISTRY[TEAMS[selectedTeam]].background }}>$FLIP</span>
+          <p className='text-md'>
+            During the entire duration of the game, each flipped tile will earn you potential $FLIP tokens. There are
+            two types of tiles: Powerup and normal tiles. Powerup tiles are rarer and unflippable, while normal tiles
+            can get flipped back by bots. <br />
+            <br />
+            Once the game is over, you will be able to claim your $FLIP tokens.
+          </p>
+          <div className='flex flex-row w-full h-full/3 items-center md:px-20'>
+            <span
+              style={{
+                color: TILE_REGISTRY[TEAMS[selectedTeam]].background,
+                textShadow: `0 0 10px ${TILE_REGISTRY[TEAMS[selectedTeam]].background},
+                            0 0 20px ${TILE_REGISTRY[TEAMS[selectedTeam]].background},
+                            0 0 30px ${TILE_REGISTRY[TEAMS[selectedTeam]].background}`,
+                animationDuration: '3s',
+              }}
+              className='text-sm md:text-md flex-1 text-center animate-pulse'
+            >
+              Earns 2x, 4x, 8x ... 32x $FLIP
+            </span>
+            <div className='ml-8 flex-1'>
+              <Canvas
+                gl={{
+                  pixelRatio: window.devicePixelRatio,
+                }}
+                className=''
+                style={{ height: '150px', width: '200px' }}
+              >
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[10, 10, 10]} intensity={1} />
+                <group rotation={[Math.PI / 3, 0, Math.PI / 8]}>
+                  <TileInstances
+                    tiles={[
+                      { address: '0x1', x: 0, y: 0, team: selectedTeam, powerup: Powerup.Multiplier, powerupValue: 1 },
+                      { address: '0x2', x: 0.9, y: 0, team: selectedTeam, powerup: Powerup.None, powerupValue: 1 },
+                    ]}
+                    position={[-0.6, -0.1, 0]}
+                    material={material}
+                    robotMaterial={robotMaterial}
+                  />
+                </group>
+                <OrthographicCamera makeDefault zoom={70} position={[0, 0, 0]} near={0} far={100000000} />
+                <EffectComposer>
+                  <Bloom
+                    kernelSize={4}
+                    intensity={1.1}
+                    luminanceThreshold={0.4}
+                    luminanceSmoothing={1.5}
+                    resolutionScale={0.1}
+                  />
+                </EffectComposer>
+              </Canvas>
+            </div>
+            <span className='text-sm md:text-md flex-1 text-center'>
+              Earns 1 <span style={{ color: TILE_REGISTRY[TEAMS[selectedTeam]].background }}>$FLIP</span>
+            </span>
+          </div>
+          <div className='h-full' />
+          <span className='text-xs opacity-80 mb-2'>
+            *Time lock check is done based on block timestamp. You might need to wait a bit after the game ends to
+            submit your claim.
           </span>
-        </div>
-        <div className='h-full' />
-        <span className='text-xs opacity-80 mb-2'>
-          *Time lock check is done based on block timestamp. You might need to wait a bit after the game ends to submit
-          your claim.
-        </span>
-        <div className='flex flex-row w-full gap-2 justify-center'>
-          <OutlineButton
-            outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
-            className='w-full md:w-1/3'
-            text='Close'
-            onClick={onClose}
-          />
-          <OutlineButton
-            outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
-            className='w-full md:w-1/3'
-            text={
-              claimed >= userScore * 1000000000000000000 && address
-                ? `Claimed ${formatE(claimed)} $FLIP`
-                : `Claim ${userScore - Number(formatE(claimed))} $FLIP`
-            }
-            disabled={Date.now() / 1000 < timeRange[1] || !address || claimed >= userScore * 1000000000000000000}
-            onClick={async () => {
-              const userTiles = Object.values(tiles)
-                .filter((tile) => tile.address === maskAddress(address))
-                .map((tile) => poseidonHash(['0x' + tile.x.toString(16), '0x' + tile.y.toString(16)]))
-
-              try {
-                const tx = await account?.execute({
-                  contractAddress: ACTIONS_ADDRESS,
-                  entrypoint: 'claim',
-                  calldata: [userTiles],
-                })
-                toast('💰 Processing your claim...')
-                onClose()
-              } catch (e) {
-                toast(`😔 Failed to claim $FLIP: ${parseError(e)}`)
+          <div className='flex flex-row w-full gap-2 justify-center'>
+            <OutlineButton
+              outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
+              className='w-full md:w-1/3'
+              text='Close'
+              onClick={onClose}
+            />
+            <OutlineButton
+              outline={TILE_REGISTRY[TEAMS[selectedTeam]].border}
+              className='w-full md:w-1/3'
+              text={
+                claimed >= userScore * 1000000000000000000 && address
+                  ? `Claimed ${formatE(claimed)} $FLIP`
+                  : `Claim ${userScore - Number(formatE(claimed))} $FLIP`
               }
-            }}
-          />
+              disabled={Date.now() / 1000 < timeRange[1] || !address || claimed >= userScore * 1000000000000000000}
+              onClick={async () => {
+                const userTiles = Object.values(tiles)
+                  .filter((tile) => tile.address === maskAddress(address))
+                  .map((tile) => poseidonHash(['0x' + tile.x.toString(16), '0x' + tile.y.toString(16)]))
+
+                try {
+                  const tx = await account?.execute({
+                    contractAddress: ACTIONS_ADDRESS,
+                    entrypoint: 'claim',
+                    calldata: [userTiles],
+                  })
+                  toast('💰 Processing your claim...')
+                  onClose()
+                  setShowSuccess(true)
+                } catch (e) {
+                  toast(`😔 Failed to claim $FLIP: ${parseError(e)}`)
+                }
+              }}
+            />
+          </div>
         </div>
-      </div>
-    </Dialog>
+      </Dialog>
+
+      <ClaimSuccessDialog
+        isOpen={showSuccess}
+        onClose={() => setShowSuccess(false)}
+        selectedTeam={selectedTeam}
+        claimed={claimed}
+        userScore={userScore}
+        tiles={tiles}
+      />
+    </>
   )
 }
 
